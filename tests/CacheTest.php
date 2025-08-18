1807065d3f83c0802f5b91c7ea0f4859c54aa54e
Kevin Pirnie <me@kpirnie.com>
2025-08-18 10:23:25 -0400

    more psr12 fixes

--- tests/CacheTest.php
+++ tests/CacheTest.php
@@ -2,7 +2,17 @@
 require_once 'vendor/autoload.php';
 
 use KPT\Cache;
+use PHPUnit\Framework\TestCase;
 
 // Your cache system is now available
 Cache::configure(['path' => '/var/cache/myapp']);
 Cache::set('key', 'value', 3600);
+
+class CacheTest extends TestCase {
+
+    public function testPlaceholder()
+    {
+        $this->assertTrue(true);
+    }
+
+}
